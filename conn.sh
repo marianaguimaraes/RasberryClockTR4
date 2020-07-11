a9fb65e82666f6339f56653d061901a159b7d868
Mariana Guimaraes <32623226+marianaguimaraes@users.noreply.github.com>
2020-07-10 21:18:43 -0300

    Add files via upload

---
+++ conn.sh
@@ -0,0 +1,3 @@
+#Criar um arquivo de script com as seguintes instrucoes
+sudo bluetoothctl power on #liga bluetooth
+sudo python /bin/bt-pan client 00:11:22:33:44:55 #Trocar o endereco de mac do seu aparelho celular
---
+++ disconn.sh
@@ -0,0 +1,2 @@
+#Criar um arquivo de script com as seguintes instrucoes
+sudo bluetoothctl power off #desliga bluetooth
